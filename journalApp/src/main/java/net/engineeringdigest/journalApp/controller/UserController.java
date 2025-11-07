62a6c52fbdd2a4d0f8d363184279af2f645dc8e4
VivekMore1011 <vivekmore1011@gmail.com>
2025-11-07 21:22:27 +0530

    Code up to date

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controller/UserController.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/UserController.java
@@ -1,46 +1,59 @@
 package net.engineeringdigest.journalApp.controller;
 
-import net.engineeringdigest.journalApp.entity.JournalEntry;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import net.engineeringdigest.journalApp.api.response.WeatherResponse;
 import net.engineeringdigest.journalApp.entity.User;
-import net.engineeringdigest.journalApp.service.JournalEntryService;
+import net.engineeringdigest.journalApp.repository.UserRepository;
 import net.engineeringdigest.journalApp.service.UserService;
-import org.bson.types.ObjectId;
+import net.engineeringdigest.journalApp.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "User APIs", description = "Read, Update & Delete User")
 public class UserController {
 
-@Autowired
-private UserService userService;
+    @Autowired
+    private UserService userService;
 
-@GetMapping
-public List<User>getAllUsers(){
-    return userService.getAll();
-}
+    @Autowired
+    private UserRepository userRepository;
 
-@PostMapping
-public void createUser(@RequestBody User user){
-    userService.saveEntry(user);
-}
+    @Autowired
+    private WeatherService weatherService;
 
-@PutMapping("/{userName}")
-    public ResponseEntity<?>updateUser(@RequestBody User user,@PathVariable String userName){
-      User userInDb=userService.findByUserName(userName);
-      if(userInDb!=null){
-          userInDb.setUserName(user.getUserName());
-          userInDb.setPassword(user.getPassword());
-          userService.saveEntry(userInDb);
-      }
-      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-}
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody User user) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
+        User userInDb = userService.findByUserName(userName);
+        userInDb.setUserName(user.getUserName());
+        userInDb.setPassword(user.getPassword());
+        userService.saveNewUser(userInDb);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteUserById() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        userRepository.deleteByUserName(authentication.getName());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> greeting() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        WeatherResponse weatherResponse = weatherService.getWeather("Mumbai");
+        String greeting = "";
+        if (weatherResponse != null) {
+            greeting = ", Weather feels like " + weatherResponse.getCurrent().getFeelslike();
+        }
+        return new ResponseEntity<>("Hi " + authentication.getName() + greeting, HttpStatus.OK);
+    }
+
 }
